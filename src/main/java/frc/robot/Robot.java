edc8d5d19fefdde1776ef00aafb3c5a3ae38e2aa
Daniel Margavio <danmargavio@gmail.com>
2023-01-11 22:27:33 -0600

    Basic example shuffleboard pull down menu

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -23,9 +25,23 @@ public class Robot extends TimedRobot {
   XboxController driver_controller = new XboxController(0);
 
   boolean driver_controller_A_button;
+  boolean driver_controller_Y_button;
+  boolean driver_controller_X_button;
+  boolean driver_controller_B_button;
+
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+
+  }
 
   // DANS BRILLANT IDEA
 
@@ -33,14 +49,36 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
 
     getControllerStates();
+    if(driver_controller_B_button)
+    {
+      // something
+    }
 
   }
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+
+  }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+
+  }
 
   @Override
   public void teleopInit() {}
@@ -70,5 +108,9 @@ public class Robot extends TimedRobot {
 
   public void getControllerStates() {
     driver_controller_A_button = driver_controller.getAButton();
+    driver_controller_B_button = driver_controller.getBButton();
+    driver_controller_X_button = driver_controller.getXButton();
+    driver_controller_Y_button = driver_controller.getYButton();
+    
   }
 }
