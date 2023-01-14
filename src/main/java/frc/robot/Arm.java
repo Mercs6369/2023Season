6d0a4703f235d8526261ff705120c5af9e953afa
JeffreyTheSuperKat <j.k.weathersbee@gmail.com>
2023-01-14 12:55:36 -0600

    test for arm foundation

--- src/main/java/frc/robot/Arm.java
+++ src/main/java/frc/robot/Arm.java
@@ -15,8 +15,16 @@ public class Arm {
 
     // Variables
     double arm_length;
-
+    
+    String eject_stage = "neutral";
+    String score_stage = "neutral";
+    String pickup_stage = "neutral";
+    
+    boolean movement_finished = false;
+    // If we've detected we've finished a stage, set movement_finished to true.
+    
     private final I2C.Port i2cPort = I2C.Port.kOnboard;
+
 
     // Motors
     WPI_TalonFX claw_motor = new WPI_TalonFX(0, "rio");
@@ -26,21 +34,52 @@ public class Arm {
 
     ColorSensorV3 claw_sensor = new ColorSensorV3(i2cPort);
 
+
+
     public Arm() {
 
     }
     
+    private void update_stage(String current_action, String current_stage) {
 
-    public void score_game_piece() {
-        // Scoring game pieces
     }
+
+
+
+    // Arm functions
+    
+    public void score_game_piece() {}
+
 
     public void eject_game_piece() {
-        // put eject code here
+        // movement_finished needs to be set to "true" at any point when robot is ready to go to the next stage :)
+        movement_finished = false;
+
+        if (eject_stage == "neutral" || eject_stage == "stage_one") {
+            pickup_stage = "stage_one";
+ 
+        } else if (eject_stage == "stage_two") {
+
+        } else if (eject_stage == "stage_three") {
+            
+        } else if (eject_stage == "stage_four") {
+            
+        } else if (eject_stage == "stage_five") {
+            
+        } else if (eject_stage == "stage_six") {
+            
+        }
+
+        
+        
+        
+        // This is independent from any if loops
+        if (movement_finished) {
+            update_stage("eject", eject_stage);
+        }
     }
 
-    public void pickup_game_piece() {
-        // put pickup code here  
-    }
+
+    public void pickup_game_piece() {}
 
 }
