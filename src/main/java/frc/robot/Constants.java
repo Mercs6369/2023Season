181da50779ff34e044092a56ab48da2e7dff2eea
GargiGarg <gargixgarg@gmail.com>
2023-01-14 11:32:23 -0600

    Created Constants File

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,66 @@
+package frc.robot;
+
+public class Constants {
+
+    /* IDS */
+
+    public static final int INTAKE_MOTOR_1_ID = 1;
+    public static final int INTAKE_MOTOR_2_ID = 2;
+    public static final int LIFT_MOTOR_1_ID = 3;
+    public static final int LIFT_MOTOR_2_ID = 4;
+    public static final int LIFT_MOTOR_3_ID = 14;
+    public static final int LIFT_PIVOT_MOTOR_ID = 5;
+    public static final int DRIVE_FL_MOTOR_1_ID = 6;
+    public static final int DRIVE_FL_MOTOR_2_ID = 7;
+    public static final int DRIVE_FR_MOTOR_1_ID = 8;
+    public static final int DRIVE_FR_MOTOR_2_ID = 9;
+    public static final int DRIVE_BL_MOTOR_1_ID = 10;
+    public static final int DRIVE_BL_MOTOR_2_ID = 11;
+    public static final int DRIVE_BR_MOTOR_1_ID = 12;
+    public static final int DRIVE_BR_MOTOR_2_ID = 13;
+
+    /* Drivetrain Constants */
+    
+   
+
+
+    /* Swerve Kinematics */
+
+    /* Module Gear Ratios */
+
+    /* Motor Inverts */
+
+    /* Angle Encoder Invert */
+
+    /* Swerve Current Limiting */
+
+    /* Angle Motor PID Values */
+
+    /* Drive Motor PID Values */
+
+    /* Drive Motor Characterization Values */
+
+    /* Swerve Profiling Values */
+    /* Meters per Second */
+
+
+    /* Neutral Modes */
+
+
+    /* Module Specific Constants */
+    /* Front Left Module - Module 0 */
+   
+
+
+    /* Front Right Module - Module 1 */
+
+
+    /* Back Left Module - Module 2 */
+
+
+    /* Back Right Module - Module 3 */
+
+        
+    }
+    
+
