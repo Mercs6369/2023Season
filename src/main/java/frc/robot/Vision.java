c0032969bdf93f9887ecb562ffdaec6755f4fbe8
Gaurav <salangau000@gmail.com>
2023-01-14 11:36:34 -0600

    vision changes

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -1,15 +1,16 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import java.util.List;
+
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonTrackedTarget;
 import org.photonvision.targeting.PhotonPipelineResult;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Vision extends SubsystemBase {
+public class Vision {
     PhotonCamera camera = new PhotonCamera("photonvision");
     boolean hasTargets;
     PhotonPipelineResult result;
@@ -20,8 +21,7 @@ public class Vision extends SubsystemBase {
 
     double range = 0.0;
 
-    @Override
-    public void periodic() {
+    public void targeting() {
         var result = camera.getLatestResult();
         hasTargets = result.hasTargets();
         if (hasTargets) {
@@ -30,18 +30,43 @@ public class Vision extends SubsystemBase {
 
         if (result.hasTargets()) {
             // First calculate range
-            range =
-                    PhotonUtils.calculateDistanceToTargetMeters(
+            range = PhotonUtils.calculateDistanceToTargetMeters(
                             CAMERA_HEIGHT_METERS,
                             TARGET_HEIGHT_METERS,
                             CAMERA_PITCH_RADIANS,
                             ((result.getBestTarget().getPitch())*(Math.PI/180)));
+            this.result = result;
+            
+
+            
+
     
         }
 
         SmartDashboard.putNumber("Range", range);
+        SmartDashboard.putNumber("ID", getBestTarget());
+        SmartDashboard.putNumber("Yaw", getTargetWithID(getBestTarget()).getYaw());
+        SmartDashboard.putNumber("Skew", getTargetWithID(getBestTarget()).getSkew());
         
     }
 
+    public PhotonTrackedTarget getTargetWithID(int id) { 
+        List<PhotonTrackedTarget> targets = result.getTargets(); 
+        for (PhotonTrackedTarget i : targets) {
+            if (i.getFiducialId() == id) { 
+                return i; 
+            }
+        }
+        return null; 
+    }
+
+    public int getBestTarget() {
+        if (hasTargets) {
+        return result.getBestTarget().getFiducialId(); 
+        }
+        else {
+            return -1; 
+        }
+    }
 
 }
