b4df707add67d30b86c7b01cc3df9d97a4b11309
Gaurav-021 <63582549+Gaurav-021@users.noreply.github.com>
2023-01-14 14:24:53 -0600

    i2cPort known issue

--- src/main/java/frc/robot/Arm.java
+++ src/main/java/frc/robot/Arm.java
@@ -15,71 +15,32 @@ public class Arm {
 
     // Variables
     double arm_length;
-    
-    String eject_stage = "neutral";
-    String score_stage = "neutral";
-    String pickup_stage = "neutral";
-    
-    boolean movement_finished = false;
-    // If we've detected we've finished a stage, set movement_finished to true.
-    
-    private final I2C.Port i2cPort = I2C.Port.kOnboard;
 
+    //private final I2C.Port i2cPort = I2C.Port.kOnboard; //has known bug
 
     // Motors
-    WPI_TalonFX claw_motor = new WPI_TalonFX(Constants.CLAW_MOTOR_ID, "rio");
-    WPI_TalonFX pivot_motor = new WPI_TalonFX(Constants.PIVOT_MOTOR_ID, "rio");
-    WPI_TalonFX first_stage_elevator = new WPI_TalonFX(Constants.FIRST_STAGE_ELEVATOR_ID, "rio");
-    WPI_TalonFX second_stage_elevator = new WPI_TalonFX(Constants.SECOND_STAGE_ELEVATOR_ID, "rio");
+    WPI_TalonFX claw_motor = new WPI_TalonFX(0, "rio");
+    WPI_TalonFX pivot_motor = new WPI_TalonFX(1, "rio");
+    WPI_TalonFX first_stage_elevator = new WPI_TalonFX(2, "rio");
+    WPI_TalonFX second_stage_elevator = new WPI_TalonFX(3, "rio");
 
-    ColorSensorV3 claw_sensor = new ColorSensorV3(i2cPort);
-
-
+    //ColorSensorV3 claw_sensor = new ColorSensorV3(i2cPort);
 
     public Arm() {
 
     }
     
-    private void update_stage(String current_action, String current_stage) {
 
+    public void score_game_piece() {
+        // Scoring game pieces
     }
-
-
-
-    // Arm functions
-    
-    public void score_game_piece() {}
-
 
     public void eject_game_piece() {
-        // movement_finished needs to be set to "true" at any point when robot is ready to go to the next stage :)
-        movement_finished = false;
-
-        if (eject_stage == "neutral" || eject_stage == "stage_one") {
-            pickup_stage = "stage_one";
- 
-        } else if (eject_stage == "stage_two") {
-
-        } else if (eject_stage == "stage_three") {
-            
-        } else if (eject_stage == "stage_four") {
-            
-        } else if (eject_stage == "stage_five") {
-            
-        } else if (eject_stage == "stage_six") {
-            
-        }
-
-        
-        
-        
-        // This is independent from any if loops
-        if (movement_finished) {
-            update_stage("eject", eject_stage);
-        }
+        // put eject code here
     }
 
-
-    public void pickup_game_piece() {}
+    public void pickup_game_piece() {
+        // put pickup code here  
+    }
 
 }
