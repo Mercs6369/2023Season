1fcd68a9f913a5fb7159298ec81d25a24b1991d4
JeffreyTheSuperKat <j.k.weathersbee@gmail.com>
2023-01-19 18:57:27 -0600

    LED update. yes yes productive mr duck

--- src/main/java/frc/robot/LED_Signaling.java
+++ src/main/java/frc/robot/LED_Signaling.java
@@ -1,15 +1,49 @@
 package frc.robot;
-
-
-
-// don't mess with this or else you'll get whacked in the BEAN with a baguette!
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 
 public class LED_Signaling {
-    public PWMSparkMax motor = new PWMSparkMax(0);
+    
+    // Defines the Spark motor controller.
+    public PWMSparkMax Spark = new PWMSparkMax(0);
 
-    public void dolightstuff(double colorinput) {
-        motor.set(colorinput);
+    // Sets the LED pattern to Idle at startup.
+    public LED_Signaling() {
+        SetLEDS(LED_State.Idle);
     }
+
+    // I mean, it's an enum, need I say more?
+    enum LED_State {
+        Decoration, // Probably for AhTahn Mode
+        Error, // Error
+        In_Progress, // Picking up or scoring
+        Idle, // When Idle/not doing anything
+        ReadyToScore; // Item in claw/intake
+    }
+
+
+    
+    /**
+     * Sets the LED pattern.
+     *  @param Status Can be set to Decoration, Error, In_Progress, Idle, or ReadyToScore.
+     */
+    public void SetLEDS(LED_State Status) {
+        if (Status == LED_State.Decoration) {
+            Spark.set(0);
+
+        } else if (Status == LED_State.Error) {
+            Spark.set(0);
+
+        } else if (Status == LED_State.In_Progress) {
+            Spark.set(0);
+
+        } else if (Status == LED_State.Idle) {
+            Spark.set(0);
+
+        } else if (Status == LED_State.ReadyToScore) {
+            Spark.set(0);
+        }
+    }
+
 }
+
