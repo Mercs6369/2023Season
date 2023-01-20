30fc7b5ea1e30c9469d197f07ddba728732855af
Daniel Margavio <danmargavio@gmail.com>
2023-01-19 22:32:22 -0600

    Just minor cleanups

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -1,22 +1,13 @@
 package frc.robot;
 
-
 import com.ctre.phoenix.sensors.PigeonIMU;
-
-import frc.robot.SwerveX_Module;
-
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-//import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-
 
 public class Drivetrain {
     public SwerveDriveOdometry swerveOdometry;
@@ -32,8 +23,6 @@ public class Drivetrain {
                 new Translation2d(-wheelBase / 2.0, trackWidth / 2.0), // backLeftModule or backRightModule
                 new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0)); // backLeftModule or backRightModule
 
-
-
     public Drivetrain() {
         /*
         frontLeftModule.setName("Front Left");
@@ -44,17 +33,9 @@ public class Drivetrain {
         gyro = new PigeonIMU(pigeonID);
         gyro.configFactoryDefault();
         gyro.setYaw(0);
-        
         //swerveOdometry = new SwerveDriveOdometry(swerveKinematics, new Rotation2d(gyro.getYaw()*(Math.PI/180)), null);
 
         mSwerveMods = new SwerveX_Module[] {
-
-            /*
-            int driveMotorChannel,
-            int steerMotorChannel,
-            int angleEncoderChannel
-            double offset
-            */
             new SwerveX_Module(15, 7, 12, -90.87890625), //frontLeftModule
             new SwerveX_Module(9, 8, 13, -213.75), //frontRightModule
             new SwerveX_Module(3, 2, 10, -69.785), //backLeftModule
@@ -62,9 +43,8 @@ public class Drivetrain {
         };
     }
 
-
     public void drive(Translation2d translation, double rotation, boolean fieldOriented) {
-        //rotation *= 2.0 / Math.hypot(wheelBase, trackWidth);
+        rotation *= 2.0 / Math.hypot(wheelBase, trackWidth);
         ChassisSpeeds speeds;
         if (fieldOriented) {
             speeds = ChassisSpeeds.fromFieldRelativeSpeeds(translation.getX(), translation.getY(), rotation,
@@ -86,9 +66,7 @@ public class Drivetrain {
         mSwerveMods[3].setDesiredState(states[3]);
         SmartDashboard.putNumber("Back Right Speed", states[1].speedMetersPerSecond);
         SmartDashboard.putNumber("Back Right Angle", states[1].angle.getRadians());
-
         //swerveOdometry.update(Rotation2d.fromDegrees(gyro.getYaw()), states); 
-        
     }
 
     public void update(){
@@ -96,7 +74,5 @@ public class Drivetrain {
         mSwerveMods[1].update();
         mSwerveMods[2].update();
         mSwerveMods[3].update();
-
     }
-
 }
