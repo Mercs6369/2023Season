549b51c52a883b93886ea9af76b091902f27b9bb
Daniel Margavio <danmargavio@gmail.com>
2023-02-08 18:00:36 -0600

    added some basic elevator constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,13 +14,13 @@ public class Constants {
 
     public static final double stickDeadband = 0.1;
 
-    /* IDS */
+    /* MOTOR IDS */
 
     public static final int INTAKE_MOTOR_1_ID = 21;
     public static final int CLAW_MOTOR_ID = 22;
     public static final int INTAKE_MOTOR_2_ID = 23;
-    public static final int FIRST_STAGE_ELEVATOR_ID= 24;
-    public static final int SECOND_STAGE_ELEVATOR_ID = 25;
+    public static final int ELEVATOR_MOTOR_1_ID = 29;
+    public static final int ELEVATOR_MOTOR_2_ID = 30;
     public static final int PIVOT_MOTOR_ID = 26;
     public static final int DRIVE_FL_MOTOR_1_ID = 6;
     public static final int angleMotor1ID = 15;
@@ -34,16 +34,6 @@ public class Constants {
     public static final int DRIVE_BR_MOTOR_1_ID = 12;
     public static final int angleMotor4ID = 18;
     public static final int DRIVE_BR_MOTOR_2_ID = 13;
-
-    /* Drivetrain Constants */
-
-    /* Swerve Kinematics */
-
-    /* Module Gear Ratios */
-
-    /* Motor Inverts */
-
-    /* Angle Encoder Invert */
 
     /* Swerve Current Limiting */
 
@@ -64,27 +54,12 @@ public class Constants {
     public static final double openLoopRamp = 0;
     public static final double closedLoopRamp = 0;
 
+    /* Elevator Constants */
 
-    /* Angle Motor PID Values */
-
-    /* Drive Motor PID Values */
-
-    /* Drive Motor Characterization Values */
-
-    /* Swerve Profiling Values */
-    /* Meters per Second */
-
-    /* Neutral Modes */
-
-    /* Module Specific Constants */
-    /* Front Left Module - Module 0 */
-
-    /* Front Right Module - Module 1 */
-
-    /* Back Left Module - Module 2 */
-
-    /* Back Right Module - Module 3 */
-
+    public static final double elevator_motor_gearbox_ratio = 62/8;   // 62 teeth to 8 teeth
+    public static final int elevator_motor_shaft_encoder_PWM_channel = 0;
+    public static final double elevator_encoder_lower_position_limit = 0.1;   // ???
+    public static final double elevator_encoder_upper_position_limit = 2.3;   // ???
 
     public static final class Swerve {
         public static final int pigeonID = 16;
