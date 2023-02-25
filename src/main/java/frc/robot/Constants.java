bf4881827b0053fe3d7a334839da2c5b818d1c1c
Amal <a5am.2k6@gmail.com>
2023-02-25 11:47:59 -0600

    Added Limelight Vision Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,7 +28,11 @@ public class Constants {
     public static final double idealCubeArea = 15;
 
 
-
+    /* Cone Scoring Node Constants || Used for Limelight Vision */
+    public static final double kDistanceBetweenNodes = 23.625;
+    public static final double lLimelightHeight = 0;
+    public static final double tLowerTapeHeight = 21.75;
+    
     /* MOTOR IDS */
     public static final int INTAKE_MOTOR_1_ID = 21;
     public static final int CLAW_MOTOR_ID = 22;
